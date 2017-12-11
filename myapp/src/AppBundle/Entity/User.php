5db4ff6c7c34fb65c590175dc2106f7f528e4689
me <webmaster@emolio.is>
2017-12-11 17:49:35 +0100

    till access denied, register works with login

--- myapp/src/AppBundle/Entity/User.php
+++ myapp/src/AppBundle/Entity/User.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
     /**
     * @ORM\Table(name="app_users")
+    * @ORM\Entity()
     * @UniqueEntity(fields="email", message="Email already taken")
     * @UniqueEntity(fields="username", message="Username already taken")
     */
